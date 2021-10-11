ce32ee99d2ce772f5e87c5905da9441b6b7305b4
Dmitrii <bankersenator@gmail.com>
2021-10-11 09:15:51 +0000

    sepolicy: Adapt paths for VoltageOS
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>

--- atv/sepolicy.mk
+++ atv/sepolicy.mk
@@ -5,8 +5,8 @@
 
 ifneq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/atv/vendor
+    device/voltage/sepolicy/atv/vendor
 endif
 
 PRODUCT_PRIVATE_SEPOLICY_DIRS += \
-   device/lineage/sepolicy/atv/private
+   device/voltage/sepolicy/atv/private
